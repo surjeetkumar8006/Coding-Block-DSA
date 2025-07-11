11508db58c4e23d27343573097b0e091d35dfa80
surjeet kumar <surjeetyadav80067@gmail.com>
2025-07-11 16:08:32 +0530

    Sorting And Binary Search

--- Lec8/Aggressive_cows.java
+++ Lec8/Aggressive_cows.java
@@ -4,14 +4,19 @@ import java.util.*;
 public class Aggressive_cows {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int nos = sc.nextInt();
-        int noc = sc.nextInt();
-        int[] stall = new int[nos];
-        for (int i = 0; i < stall.length; i++) {
-            stall[i] = sc.nextInt();
+        int t = sc.nextInt();
+
+        while (t > 0) {
+            int nos = sc.nextInt();
+            int noc = sc.nextInt();
+            int[] stall = new int[nos];
+            for (int i = 0; i < stall.length; i++) {
+                stall[i] = sc.nextInt();
+            }
+            Arrays.sort(stall);
+            System.out.println(largest_minimum(stall, noc));
+            t--;
         }
-        Arrays.sort(stall);
-        System.out.println(largest_minimum(stall, noc));
     }
 
     public static int largest_minimum(int[] stall, int noc) {
@@ -33,15 +38,15 @@ public class Aggressive_cows {
 
     public static boolean isItPossible(int[] stall, int noc, int mid) {
 
-        int pos=stall[0];
-        int cow=1;
-        for(int i=1;i<stall.length;i++){
-            if(stall[i]-pos>=mid){
+        int pos = stall[0];
+        int cow = 1;
+        for (int i = 1; i < stall.length; i++) {
+            if (stall[i] - pos >= mid) {
                 cow++;
-                pos=stall[i];
+                pos = stall[i];
             }
-            if(cow==noc){
-                return  true;
+            if (cow == noc) {
+                return true;
             }
         }
 
